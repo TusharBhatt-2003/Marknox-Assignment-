50ef92c758bcb95142ae1111e4469ecd4515b250
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-11-26 18:21:42 +0530

    Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,49 +1,48 @@
 import React from "react";
 import portfolioData from "../data/const.js";
 
-const Footer = ({ name, footerBG }) => {
+const Footer = ({ name }) => {
   const { socialMedia } = portfolioData.personalInfo;
 
   return (
-    <footer className="bg-black">
+    <footer className="bg-black relative overflow-hidden border-8 border-black text-white pt-16 pb-28 rounded-3xl">
+      {/* SVG Background */}
       <div
-        className=" text-white pt-16 pb-28 rounded-t-3xl"
-        // style={{
-        //   backgroundImage: `url(${footerBG})`, // Background image for the whole section
-        //   backgroundSize: "cover",
-        //   backgroundPosition: "",
-        //   backgroundRepeat: "no-repeat",
-        //   filter: "contrast(5)", // Increase contrast to 150%
-        // }}
-      >
-        <div className="flex h-full flex-col gap-5 items-center">
-          {/* Social Media Links */}
-          <div className="flex gap-10 justify-center mb-5">
-            {Object.entries(socialMedia).map(([key, { link, icon }]) => {
-              // Only render if both link and icon are present
-              if (!link || !icon) return null;
+        className="absolute inset-0 overflow-hidden bg-contain"
+        dangerouslySetInnerHTML={{ __html: portfolioData.svg }}
+      ></div>
+      {/* Gradient Overlay */}
+      <div
+        className="absolute inset-0 overflow-hidden"
+        style={{
+          background:
+            "linear-gradient(to bottom, rgba(1, 1, 1, 1), rgba(0, 0, 0, 0.5))",
+        }}
+      ></div>
+      {/* Content */}
+      <div className="relative flex h-full flex-col gap-5 items-center">
+        {/* Social Media Links */}
+        <div className="flex gap-10 justify-center mb-5">
+          {Object.entries(socialMedia).map(([key, { link, icon }]) => {
+            if (!link || !icon) return null; // Only render if both link and icon are present
 
-              return (
-                <a
-                  key={key}
-                  href={link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="hover:scale-90 transition-all ease-in-out"
-                  aria-label={key}
-                >
-                  <div
-                    dangerouslySetInnerHTML={{ __html: icon }}
-                    className=""
-                  ></div>
-                </a>
-              );
-            })}
-          </div>
-          <p>
-            &copy; {new Date().getFullYear()} {name}. All Rights Reserved.
-          </p>
+            return (
+              <a
+                key={key}
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:scale-90 transition-all ease-in-out"
+                aria-label={key}
+              >
+                <div dangerouslySetInnerHTML={{ __html: icon }}></div>
+              </a>
+            );
+          })}
         </div>
+        <p>
+          &copy; {new Date().getFullYear()} {name}. All Rights Reserved.
+        </p>
       </div>
     </footer>
   );
